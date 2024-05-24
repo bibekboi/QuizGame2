9e48d6b6487cd2442da1e92beeda421309e3f09a
bibekboi <bibek.rizal1@gmail.com>
2024-05-24 19:52:41 +0545

    Added correct and incorrect
    New design edits.

--- scripts/question_1.gd
+++ scripts/question_1.gd
@@ -1,32 +1,41 @@
 extends Control
 
 enum TheCorrectAnswer { FIRST_ANSWER, SECOND_ANSWER, THIRD_ANSWER }
-
 @export var correctAnswer : TheCorrectAnswer
 
+@onready var correct_answer_screen = $CorrectAnswer
+@onready var incorrect_answer_screen = $IncorrectAnswer
+@onready var score_counter_text = $ScoreCounter_TEXT
+
+var scoreCounter : int = 0
+
 func _ready():
-	pass
+	correct_answer_screen.visible = false
+	incorrect_answer_screen.visible = false
+
+func _process(delta):
+	score_counter_text.text = str(scoreCounter)
 
 func _on_answer_1_button_down():
 	if correctAnswer == TheCorrectAnswer.FIRST_ANSWER:
-		print("Correct Answer")
+		scoreCounter += 1
+		correct_answer_screen.visible = true
 	else: 
-		print("Incorrect Answer")
-
+		incorrect_answer_screen.visible = true
 
 func _on_answer_2_button_down():
 	if correctAnswer == TheCorrectAnswer.SECOND_ANSWER:
-		print("Correct Answer")
+		scoreCounter += 1
+		correct_answer_screen.visible = true
 	else: 
-		print("Incorrect Answer")
-
+		incorrect_answer_screen.visible = true
 
 func _on_answer_3_button_down():
 	if correctAnswer == TheCorrectAnswer.THIRD_ANSWER:
-		print("Correct Answer")
+		scoreCounter += 1
+		correct_answer_screen.visible = true
 	else: 
-		print("Incorrect Answer")
-
+		incorrect_answer_screen.visible = true
 
 func _on_cross_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels_scene.tscn")
